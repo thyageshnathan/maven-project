cd782710cb42130a24962a50158e02233e33d127
thyageshnathan <thyageshnathan@gmail.com>
2018-03-07 20:39:03 -0700

    changes to greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /** Some new comment
+  **/
+  
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
